3b9819e40e68c307f49ef0fb6f58a011be17b8bc
Tadhg <oconghat@tcd.ie>
2021-11-05 23:14:35 +0000

    basic event handler

--- app/src/main/java/ie/wit/pintmark/PintmarkerActivity.kt
+++ app/src/main/java/ie/wit/pintmark/PintmarkerActivity.kt
@@ -2,10 +2,15 @@ package ie.wit.pintmark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import ie.wit.pintmark.databinding.ActivityPintmarkBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class PintmarkerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPintmarkBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pintmark)
@@ -14,5 +19,18 @@ class PintmarkerActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
         i("Pintmark Activity started..")
 
+        binding = ActivityPintmarkBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnAdd.setOnClickListener() {
+            val placemarkTitle = binding.placemarkTitle.text.toString()
+            if (placemarkTitle.isNotEmpty()) {
+                i("add Button Pressed: $placemarkTitle")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 }
