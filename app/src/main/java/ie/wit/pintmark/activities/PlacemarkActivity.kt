0d308d5807a430a88e77ed2053475a5fefb9b4f6
Tadhg <oconghat@tcd.ie>
2021-11-06 15:34:40 +0000

    implementation of MainApp class

--- app/src/main/java/ie/wit/pintmark/activities/PlacemarkActivity.kt
+++ app/src/main/java/ie/wit/pintmark/activities/PlacemarkActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import ie.wit.pintmark.databinding.ActivityPintmarkBinding
+import ie.wit.pintmark.main.MainApp
 import ie.wit.pintmark.models.PlacemarkModel
 import timber.log.Timber
 import timber.log.Timber.i
@@ -12,30 +13,27 @@ class PlacemarkActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPintmarkBinding
     var placemark = PlacemarkModel()
-    val placemarks = ArrayList<PlacemarkModel>()
+    lateinit var app : MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityPintmarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Timber.plant(Timber.DebugTree())
-
+        app = application as MainApp
         i("Placemark Activity started...")
-
         binding.btnAdd.setOnClickListener() {
             placemark.title = binding.placemarkTitle.text.toString()
             placemark.description = binding.placemarkDescription.text.toString()
             if (placemark.title.isNotEmpty()) {
-                placemarks.add(placemark.copy())
-                i("add Button Pressed: $placemark.title")
-                for (i in placemarks.indices)
-                { i("Placemark[$i]:${this.placemarks[i]}") }
+                app.placemarks.add(placemark.copy())
+                i("add Button Pressed: ${placemark}")
+                for (i in app.placemarks.indices) {
+                    i("Placemark[$i]:${app.placemarks[i]}")
+                }
             }
             else {
-                Snackbar
-                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
---
+++ app/src/main/java/ie/wit/pintmark/main/MainApp.kt
@@ -0,0 +1,17 @@
+package ie.wit.pintmark.main
+
+import android.app.Application
+import ie.wit.pintmark.models.PlacemarkModel
+import timber.log.Timber
+import timber.log.Timber.i
+
+class MainApp : Application() {
+
+    val placemarks = ArrayList<PlacemarkModel>()
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        i("Placemark started")
+    }
+}
